476b89290327e271f7de21ba2c4e1cbd66da70d9
tirathconstacloud-max <tirath.constacloud@gmail.com>
2026-02-17 14:10:15 +0530

    Add files via upload

--- my_universal_auth/my_universal_auth/hooks.py
+++ my_universal_auth/my_universal_auth/hooks.py
@@ -1,15 +1,15 @@
-app_name = "test app"
+app_name = "my_universal_auth"
 app_title = "My Universal Auth"
-app_publisher = "Your Name"
-app_description = "Universal OAuth for Multiple Sites"
-app_email = "yash@yopmail.com"
+app_publisher = "Tirath"
+app_description = "OAuth for Multiple Sites"
+app_email = "test@example.com"
 app_license = "MIT"
+app_version = "0.0.1"
 
-# Ye sabse important part hai: fixtures
-# Jab bhi app install hoga, ye OAuth Client record create kar dega
+# Fixtures for OAuth Client
 fixtures = [
     {
         "dt": "OAuth Client",
-        "filters": [["app_name", "=", "test app"]]
+        "filters": [["app_name", "=", "SPS Commerce App"]]
     }
 ]
